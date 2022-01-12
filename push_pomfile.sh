db5bf86f61b3fcd8850f08fc2bdb0dcd17f0d235
turbou <turbou@i.softbank.jp>
2022-01-13 02:04:29 +0900

    update.

--- push_pomfile.sh
+++ push_pomfile.sh
@@ -2,17 +2,16 @@
 
 AGENT_POM_FILE='pom_agent.xml'
 echo "check!!!"
-echo ${BuildVariables.AGENT_OLD_VERSION}
-echo ${BuildVariables.AGENT_NEW_VERSION}
-echo ${BuildVariables.GITHUB_USER_NAME}
-echo ${BuildVariables.GITHUB_USER_EMAIL}
+echo ${AGENT_OLD_VERSION}
+echo ${AGENT_NEW_VERSION}
 echo ${GITHUB_USER_NAME}
 echo ${GITHUB_USER_EMAIL}
 echo "check!!!"
 
 git config --global user.name ${GITHUB_USER_NAME}
 git config --global user.email ${GITHUB_USER_EMAIL}
-git commit -m "${BuildVariables.AGENT_OLD_VERSION} -> ${BuildVariables.AGENT_NEW_VERSION}." ${AGENT_POM_FILE}
+#git commit -m "${AGENT_OLD_VERSION} -> ${AGENT_NEW_VERSION}." ${AGENT_POM_FILE}
+git commit -m "update from codebuild." ${AGENT_POM_FILE}
 git push origin main
 
 exit 0
