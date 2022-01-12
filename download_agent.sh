53be35bac0769171d0928b3bd28a92bac7fc68bf
turbou <turbou@i.softbank.jp>
2022-01-13 00:18:58 +0900

    update.

--- download_agent.sh
+++ download_agent.sh
@@ -3,6 +3,7 @@
 AGENT_POM_FILE='pom_agent.xml'
 echo "check!!!"
 echo ${AGENT_OLD_VERSION}
+echo ${AGENT_NEW_VERSION}
 echo "check!!!"
 
 mvn dependency:copy-dependencies -f ${AGENT_POM_FILE}
--- push_pomfile.sh
+++ push_pomfile.sh
@@ -6,6 +6,8 @@ echo ${AGENT_OLD_VERSION}
 echo ${AGENT_NEW_VERSION}
 echo "check!!!"
 
+git config user.name ${GITHUB_USER_NAME}
+git config user.email ${GITHUB_USER_EMAIL}
 git commit -m "${AGENT_OLD_VERSION} -> ${AGENT_NEW_VERSION}." ${AGENT_POM_FILE}
 git push origin main
 
